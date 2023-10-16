f502c582e54c41090595ae16742ed9e471a08c42
Mark Mwima <markmwimar@gmail.com>
2023-10-16 09:01:48 +0300

    describe method

--- jobs.js
+++ jobs.js
@@ -4,10 +4,18 @@ class Job {
     this.place = location;
     this.pay = salary;
   }
+  describe() {
+    console.log(
+      `I am a ${this.title} I work in ${this.place} and my salary is ${this.pay}`
+    );
+  }
 }
 
 const developer = new Job("designer", "Nairobi", "200000");
 const artist = new Job("painter", "Nakuru", "100000");
 
-console.log(developer);
-console.log(artist);
+// console.log(developer);
+// console.log(artist);
+
+developer.describe();
+artist.describe();
