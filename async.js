b20e78aed63e55c9f0345f4197e168835d338bf7
Mark Mwima <markmwimar@gmail.com>
2023-10-16 23:08:08 +0300

    async prac

--- async.js
+++ async.js
@@ -1,12 +1,25 @@
-const fs = require("fs");
+const fs = require("fs/promises");
+const util = require("util");
 
-function readFile() {
+// function readFile() {
+//   let fileData;
+//   fileData = fs.readFile("data.txt", function (error, fileData) {
+//     console.log("file reading done");
+//     console.log(fileData.toString());
+//   });
+
+//   console.log("Wassup");
+// }
+
+async function readFile() {
   let fileData;
-  fileData = fs.readFile("data.txt", function () {
-    console.log("file reading done");
-    console.log(fileData.toString());
-  });
-
-  console.log("Wassup");
+  try {
+    fileData = await fs.readFile("data.txt");
+  } catch (error) {
+    console.log("error found");
+  }
+  console.log(fileData.toString());
+  console.log("parsing done");
 }
+
 readFile();
